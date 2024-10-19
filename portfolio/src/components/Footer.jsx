0d02a323108757d17cc26a92c6090f20d0cd1c98
Vishal Sohaliya <vishalsohaliya25@gmail.com>
2024-10-19 22:44:31 +0530

    "Updated LinkedIn profile URL in Footer component"

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="fixed top-1/3 left-0 space-y-4">
-      <a href="https://www.linkedin.com/in/vishal-sohaliya" className="block p-3 bg-gray-800 text-white">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/vishal-sohaliya-a177b2277/" className="block p-3 bg-gray-800 text-white">LinkedIn</a>
       <a href="https://github.com/vills25" className="block p-3 bg-gray-800 text-white">GitHub</a>
       <a href="https://app.daily.dev/vishalsohaliya" className="block p-3 bg-gray-800 text-white">DailyDevApp</a>
     </div>
