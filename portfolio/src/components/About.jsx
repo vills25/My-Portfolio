3a7ca0e8d1cbb535ddf1bc1f0ceaeb6d2e798cb5
Vishal Sohaliya <vishalsohaliya25@gmail.com>
2024-12-30 20:22:40 +0530

    Design changed

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -3,11 +3,11 @@ import './About.css'
 
 const About = () => {
   return (
-    <section id="about" className="min-h-screen bg-gray-100 py-14">
+    <section id="about" className="min-h-screen bg-gradient-to-r from-[rgb(53,52,50)] to-[rgb(0,0,0)] py-14">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-8 mt-16">About Me</h2>
+        <h2 className="text-4xl font-bold text-center mb-8 mt-16 text-blue-400">About Me</h2>
 
-        <p className="text-lg text-center text-gray-600 mb-8">
+        <p className="text-lg text-center text-gray-400 mb-8">
           Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology.
         </p>
 
@@ -15,8 +15,8 @@ const About = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-start">
           {/* Left Column: Introduction */}
           <div>
-            <h3 className="text-2xl font-bold mb-3 ml-5">Get to know me!</h3>
-            <p className="text-lg text-gray-700 leading-relaxed p-5  ml-10 p-10;">
+            <h3 className="text-2xl text-teal-100 font-bold mb-3 ml-5">Get to know me!</h3>
+            <p className="text-lg text-red-600 leading-relaxed p-5  ml-10 p-10;">
               I have learned ReactJS Development and Python development with Django Framework and REST API Integration. I have a background in React Development and Python Development along with MySQL Database and Javascript. I am passionate about leveraging my skills to create innovative solutions and contribute to the I.T. industry. I took an offline course to learn more about Web Development and successfully finished the course with a good score.
             </p>
           </div>
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -5,9 +5,11 @@ import { SiDailydotdev } from "react-icons/si";
 
 const Home = () => {
   return (
-    <section id="home" className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-900 via-blue-600 to-blue-400">
+    <section id="home" className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-[#FFDAB9] to-[#008080]">
       <div className="Introparagraph text-center">
-        <h2 className="text-5xl font-bold gradient-text">HEY, I'M VISHAL SOHALIYA</h2>
+        <h2 className="text-5xl font-bold gradient-text bg-gradient-to-r from-[#003366] to-[#FF6600]">
+          HEY, I'M VISHAL SOHALIYA
+        </h2>
       </div>
 
       <div className="Iam">
@@ -25,15 +27,20 @@ const Home = () => {
       </p>
 
       <div className="flex space-x-4 mt-5">
-        <a href="https://www.linkedin.com/in/vishalsohalia-a177b2277/" className="social-button flex items-center">
+        <a href="https://www.linkedin.com/in/vishalsohalia-a177b2277/" className="social-button flex items-center hover:bg-cyan-500">
           <FaLinkedin className="mr-2" /> LinkedIn
         </a>
         <a href="https://github.com/vills25" className="social-button flex items-center">
           <FaGithub className="mr-2" /> GitHub
         </a>
-        <a href="https://app.daily.dev/vishalsohaliya" className="social-button flex items-center">
+        <a href="https://app.daily.dev/vishalsohaliya" className="social-button flex items-center hover:bg-fuchsia-800">
           <SiDailydotdev className="mr-2" /> DevApp
         </a>
+      </div>
+      <div className="wave-svg absolute bottom-0 w-full">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#00f260" fill-opacity="1" d="M0,64L80,96C160,128,320,192,480,202.7C640,213,800,171,960,160C1120,149,1280,171,1360,181.3L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+        </svg>
       </div>
     </section>
   );
